636664b608bfd7bfcfeaa4a14b8e7796c8137143
liuzenglu <liuzenglu@duoduosports.com>
2017-04-18 21:10:07 +0800

    fix

--- tensor-reinforcement/dqn_model.py
+++ tensor-reinforcement/dqn_model.py
@@ -36,9 +36,9 @@ class DQN(object):
         checkpoint = tf.train.get_checkpoint_state("saved_networks")
         if checkpoint and checkpoint.model_checkpoint_path:
                 self.saver.restore(self.session, checkpoint.model_checkpoint_path)
-                print "Successfully loaded:", checkpoint.model_checkpoint_path
+                print("Successfully loaded:", checkpoint.model_checkpoint_path)
         else:
-                print "Could not find old network weights"
+                print("Could not find old network weights")
 
         global summary_writer
         summary_writer = tf.train.SummaryWriter('logs', graph=self.session.graph)
